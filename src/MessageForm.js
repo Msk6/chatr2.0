543eda0fbb73abaa635cad3258dc65121e1e0206
Msk6 <Maha.s.kh@hotmail.com>
2020-10-22 17:23:31 +0300

    emoji keyboard

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { PostMessages } from "./redux/actions";
 
+//Emoji
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
+
 const MessageForm = (props) => {
+  const [viewEmojis, setViewEmojis] = useState(false)
   const [userData, setUserData] = useState({
     message: "",
   });
@@ -18,7 +23,19 @@ const MessageForm = (props) => {
     resetValue();
   };
 
+  
+
   const { message } = userData;
+
+  const addEmoji = emojiObj => {
+    let emoji = emojiObj.native
+    setUserData({...userData, message: message+emoji})
+  }
+
+  const handleViewEmojis = () =>{
+    setViewEmojis(!viewEmojis)
+  }
+  
 
   return (
     <div className="col-6 mx-auto">
@@ -27,6 +44,10 @@ const MessageForm = (props) => {
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="message">Message</label>
+              <div className="input-group">
+                <div className="input-group-append">
+                <button className="btn btn-outline-secondary" onClick={handleViewEmojis}>emojis</button>
+                </div>
               <input
                 type="text"
                 className="form-control"
@@ -37,17 +58,23 @@ const MessageForm = (props) => {
                 onChange={handleChange}
               />
             </div>
-
+            </div>
             <button type="submit" className="btn btn-primary">
               send
             </button>
           </form>
+          {
+          viewEmojis?
+          <span>
+          <Picker onSelect={addEmoji}/>
+          </span>:null
+          }
+          
         </div>
       </div>
     </div>
   );
 };
-// const mapStateToProps = ({ channels }) => ({ channels });
 const mapDispatchToProps = (dispatch) => {
   return {
     PostMessages: (channelID, userData) =>
